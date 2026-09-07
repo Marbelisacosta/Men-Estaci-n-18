49f9d51f6dd678a2a852c531124ebcb7c0d251f2
marbelis acosta <pixel.neo07@gmail.com>
2026-09-07 14:39:13 +0000

    AREPA CON PROTEÍNA 1.50$ WHOPPER 4.00$ JUNIOR 2.00$ SALCHIQUESO 2.00$ PA

--- src/lib/menu-data.ts
+++ src/lib/menu-data.ts
@@ -42,42 +42,42 @@ export const menuItems: MenuItem[] = [
   {
     id: 'fast-2',
     name: 'WHOPPER',
-    price: 3.00,
+    price: 4.00,
     category: 'fast-food',
     image: 'burger'
   },
   {
     id: 'fast-3',
     name: 'JUNIOR',
-    price: 1.50,
+    price: 2.00,
     category: 'fast-food',
     image: 'burger'
   },
   {
     id: 'fast-4',
     name: 'SALCHIQUESO',
-    price: 1.50,
+    price: 2.00,
     category: 'fast-food',
     image: 'hotdog'
   },
   {
     id: 'fast-5',
     name: 'PATACON PERSONAL',
-    price: 2.00,
+    price: 3.00,
     category: 'fast-food',
     image: 'patacon'
   },
   {
     id: 'fast-6',
     name: 'MINI PATACÓN',
-    price: 2.00,
+    price: 3.00,
     category: 'fast-food',
     image: 'patacon'
   },
   {
     id: 'fast-7',
     name: 'PAN DE SALCHICHA',
-    price: 4.00,
+    price: 1.50,
     category: 'fast-food',
     image: 'hotdog'
   },

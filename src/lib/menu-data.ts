89d220469e568ea57f2a025866a27e8e5cf29201
marbelis acosta <pixel.neo07@gmail.com>
2026-09-24 20:29:22 +0000

    PAPAS FRITAS CON SALCHICHA Y SALSAS

--- src/lib/menu-data.ts
+++ src/lib/menu-data.ts
@@ -59,7 +59,7 @@ export const menuItems: MenuItem[] = [
   {
     id: 'fast-salchipapa-tradicional',
     name: 'SALCHIPAPA TRADICIONAL',
-    description: 'PAPAS FRITAS CON SALCHICHA, CEBÚ, JAMON, VEGETALES Y SALSAS',
+    description: 'PAPAS FRITAS CON SALCHICHA Y SALSAS',
     price: 3.00,
     category: 'fast-food',
     image: 'salchipapa-tradicional-img',
